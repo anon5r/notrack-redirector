32991a676a4f1e593ce1a1eeb88561fa9a3d7644
anon <anon@anoncom.net>
2023-04-08 02:45:47 +0900

    Fix redirect target detection

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"log"
 )
 
 var specURL = map[string]string{
@@ -23,30 +24,42 @@ func main() {
 	// 「/」に対して処理を追加
 	mux.HandleFunc("/", redirector)
 
-	// 8080ポートで起動
+	// Start port as 9000
 	http.ListenAndServe(":9000", mux)
 }
 
 // redirector
 func redirector(w http.ResponseWriter, r *http.Request) {
 
+	if r.URL.Path == "/favicon.ico" {
+		return
+	}
+
 	// No query
 	if r.URL.Query() != nil {
+		queries := r.URL.Query()
+		log.Printf("Request: ", r.Host, r.URL.Path, queries.Encode())
+
 		// Specified domains
 		if specParam, ok := specURL[r.Host]; ok {
-			if len(specParam) > 6 && (strings.HasPrefix(specParam, "http://") || strings.HasPrefix(specParam, "https://")) {
+			if len(specParam) > 1 {
+				r.URL.Query()
+				log.Printf("param = %+v", specParam)
 				redirect := r.URL.Query().Get(specParam)
-				w.Header().Set("Location", redirect)
-				w.WriteHeader(http.StatusFound)
-				return
+				log.Printf("value = %+v", redirect)
+				if (len(redirect) > 5 && strings.HasPrefix(redirect, "http://") || strings.HasPrefix(redirect, "https://")) {	
+					log.Printf("redirect => ", redirect)
+					w.Header().Set("Location", redirect)
+					w.WriteHeader(http.StatusFound)
+					return
+				}
 			}
 		}
 
 		// Common
-		queries := r.URL.Query()
 		for _, key := range commonParams {
 			if param, ok := queries[key]; ok {
-				// log.Printf("param = %+v", param)
+				log.Printf("param = %+v", param)
 				// log.Printf("param.last! = %+v", param[len(param)-1])
 				redirect := param[len(param)-1]
 				w.Header().Set("Location", redirect)
